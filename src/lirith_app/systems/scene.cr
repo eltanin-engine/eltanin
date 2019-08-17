ef1fbb0f7342f76900632512917d2854a355861d
Max Berends <berendsmax@gmail.com>
2019-08-17 20:37:19 +0200

    Use r_image for texture image

--- src/lirith_app/systems/scene.cr
+++ src/lirith_app/systems/scene.cr
@@ -2,10 +2,14 @@ module LirithApp
   module Systems
     class Scene < Lirith::Systems::Base
       def initialize
+        texture = Lirith::Texture.new(
+          image: RImage::Formats::Bitmap.new(File.open("./src/lirith_app/textures/uvtemplate.bmp"))
+        )
+
         @model = Models::Cube.new
         loader = Lirith::Loaders::JsonLoader.new
         mesh = loader.load(File.open("./src/lirith_app/models/cube.json"))
-        mesh.texture = Lirith::Texture.new(File.open("./src/lirith_app/textures/uvtemplate.bmp"))
+        mesh.texture = texture
         Lirith.application.scene.children << mesh
 
         mesh2 = mesh.clone
@@ -19,12 +23,13 @@ module LirithApp
         mesh3.update_view
         Lirith.application.scene.children << mesh3
 
-        plane = Lirith::Objects::Plane.create(2, 2, 1, 1)
+        plane = Lirith::Objects::Plane.create(2, 2, 2, 2)
         #plane.rotate_x Lirith::Math.deg2rad(90.0)
         plane.translate_z 6
         plane.translate_x -1
         plane.translate_y -1
         plane.update_view
+        plane.texture = texture
         Lirith.application.scene.children << plane
       end
 
