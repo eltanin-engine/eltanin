44161cf19159e6fdc6355d0bc57e2c13946e1a6b
Max Berends <berendsmax@gmail.com>
2019-08-23 17:43:36 +0200

    Add and load obj cube

--- src/lirith_app/systems/scene.cr
+++ src/lirith_app/systems/scene.cr
@@ -10,18 +10,18 @@ module LirithApp
         loader = Lirith::Loaders::JsonLoader.new
         mesh = loader.load(File.open("./src/lirith_app/models/cube.json"))
         mesh.texture = texture
-        Lirith.application.scene.children << mesh
+        #Lirith.application.scene.children << mesh
 
         mesh2 = mesh.clone
         # mesh2 = loader.load(File.open("./src/lirith_app/models/cube.json"))
         mesh2.position.x -= 2
         mesh2.update_view
-        Lirith.application.scene.children << mesh2
+        #Lirith.application.scene.children << mesh2
 
         mesh3 = loader.load(File.open("./src/lirith_app/models/cube.json"))
         mesh3.position.x += 2
         mesh3.update_view
-        Lirith.application.scene.children << mesh3
+        #Lirith.application.scene.children << mesh3
 
         plane = Lirith::Objects::Plane.create(2, 2, 2, 2)
         #plane.rotate_x Lirith::Math.deg2rad(90.0)
@@ -30,7 +30,12 @@ module LirithApp
         plane.translate_y -1
         plane.update_view
         plane.texture = texture
-        Lirith.application.scene.children << plane
+        #Lirith.application.scene.children << plane
+
+        obj_loader = Lirith::Loaders::ObjLoader.new
+        obj_mesh = obj_loader.load(File.open("./src/lirith_app/models/cube.obj"))
+        obj_mesh.texture = texture
+        Lirith.application.scene.children << obj_mesh
       end
 
       def handle_event(event)
